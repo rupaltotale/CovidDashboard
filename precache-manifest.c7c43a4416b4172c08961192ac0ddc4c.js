c9a4b31a819e07d68a379e52aabde161ff426db6
Rupal Totale <rupaltotale@gmail.com>
2020-10-26 16:38:56 -0700

    Updates

---
+++ precache-manifest.c7c43a4416b4172c08961192ac0ddc4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066e4835d1074487d1e4410d75db8065",
+    "url": "/CovidDashboard/index.html"
+  },
+  {
+    "revision": "fe1a8484fb397b887bb9",
+    "url": "/CovidDashboard/static/js/2.73f614a9.chunk.js"
+  },
+  {
+    "revision": "aa8492f8a2170b75c583d9f251ac1beb",
+    "url": "/CovidDashboard/static/js/2.73f614a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "927a76556e1903502247",
+    "url": "/CovidDashboard/static/js/main.818c16e9.chunk.js"
+  },
+  {
+    "revision": "dd2c7b89d9f411f638a6",
+    "url": "/CovidDashboard/static/js/runtime-main.fe56f81c.js"
+  }
+]);
