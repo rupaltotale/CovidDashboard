fc2e494f5c5ea45aed2b7a9f1dd199050a7ade69
Rupal Totale <rupaltotale@gmail.com>
2020-11-16 01:30:07 -0800

    all graphs working, date range working, only breakdown for deaths pending

--- api/src/routes.py
+++ api/src/routes.py
@@ -5,15 +5,20 @@ from src import db
 from src.models import RaceEntry
 from sqlalchemy import func
 from flask import jsonify
+from flask import request
+import datetime
 
 
 @app.route('/get-total-cases-by-state')
-def get_total_cases_by_state():
+def get_data_by_state():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
     results = db.session.query(RaceEntry.state,
                                func.sum(RaceEntry.cases_total),
                                func.sum(RaceEntry.deaths_total),
                                ).group_by(RaceEntry.state
-                                          ).order_by(func.sum(RaceEntry.cases_total).desc()
+                                          ).filter(RaceEntry.date >= start_date
+                                                   ).filter(RaceEntry.date <= end_date).order_by(func.sum(RaceEntry.cases_total).desc()
                                                      ).limit(10)
 
     return jsonify(
@@ -23,23 +28,30 @@ def get_total_cases_by_state():
 
 
 @app.route('/get-total-cases-by-date')
-def get_total_cases_by_date():
+def get_data_by_date():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
     results = db.session.query(RaceEntry.date,
                                func.sum(RaceEntry.cases_total),
                                func.sum(RaceEntry.deaths_total),
-                               ).group_by(RaceEntry.date)
+                               ).group_by(RaceEntry.date
+                                          ).filter(RaceEntry.date >= start_date
+                                                                 ).filter(RaceEntry.date <= end_date)
     return jsonify(
         date=[e[0] for e in results.all()],
         cases=[e[1] for e in results.all()],
         deaths=[e[2] for e in results.all()],)
 
 
-@app.route('/get-total-cases-by-date-for-state')
-def get_total_cases_by_date_for_state():
+@app.route('/get-total-cases-by-date-for-state/<string:state>')
+def get_data_by_date_for_state(state):
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
     results = db.session.query(RaceEntry.date,
                                func.sum(RaceEntry.cases_total),
                                func.sum(RaceEntry.deaths_total),
-                               ).group_by(RaceEntry.date)
+                               ).group_by(RaceEntry.date).filter(RaceEntry.date >= start_date
+                                                                 ).filter(RaceEntry.date <= end_date)
     return jsonify(
         date=[e[0] for e in results.all()],
         cases=[e[1] for e in results.all()],
@@ -48,12 +60,15 @@ def get_total_cases_by_date_for_state():
 
 @app.route('/get-cases-by-date-and-race')
 def get_cases_by_date_and_race():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
     results = db.session.query(RaceEntry.date,
                                func.sum(RaceEntry.cases_white),
                                func.sum(RaceEntry.cases_black),
                                func.sum(RaceEntry.cases_latino),
                                func.sum(RaceEntry.cases_multiracial)
-                               ).group_by(RaceEntry.date)
+                               ).group_by(RaceEntry.date).filter(RaceEntry.date >= start_date
+                                                                 ).filter(RaceEntry.date <= end_date)
     return jsonify(
         date=[e[0] for e in results.all()],
         white=[e[1] for e in results.all()],
@@ -61,17 +76,49 @@ def get_cases_by_date_and_race():
         latino=[e[3] for e in results.all()],
         multi=[e[4] for e in results.all()])
 
+@app.route('/get-cases-by-state-and-race')
+def get_cases_by_state_and_race():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    results = db.session.query(RaceEntry.state,
+                               func.sum(RaceEntry.cases_white),
+                               func.sum(RaceEntry.cases_black),
+                               func.sum(RaceEntry.cases_latino),
+                               func.sum(RaceEntry.cases_multiracial)
+                               ).group_by(RaceEntry.state).filter(RaceEntry.date >= start_date
+                                                                 ).filter(RaceEntry.date <= end_date).order_by(func.sum(RaceEntry.cases_total).desc()
+                                                     ).limit(10)
+    return jsonify(
+        state=[e[0] for e in results.all()],
+        white=[e[1] for e in results.all()],
+        black=[e[2] for e in results.all()],
+        latino=[e[3] for e in results.all()],
+        multi=[e[4] for e in results.all()])
+
+
 @app.route('/get-deaths-by-date-and-race')
 def get_deaths_by_date_and_race():
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
     results = db.session.query(RaceEntry.date,
                                func.sum(RaceEntry.deaths_white),
                                func.sum(RaceEntry.deaths_black),
                                func.sum(RaceEntry.deaths_latino),
                                func.sum(RaceEntry.deaths_multiracial)
-                               ).group_by(RaceEntry.date)
+                               ).group_by(RaceEntry.date).filter(RaceEntry.date >= start_date
+                                                                 ).filter(RaceEntry.date <= end_date)
     return jsonify(
         date=[e[0] for e in results.all()],
         white=[e[1] for e in results.all()],
         black=[e[2] for e in results.all()],
         latino=[e[3] for e in results.all()],
         multi=[e[4] for e in results.all()])
+
+
+@app.route('/get-date-range')
+def get_date_range():
+    results = db.session.query(func.max(RaceEntry.date),
+                               func.min(RaceEntry.date))
+    return jsonify(
+        start_date=[e[1] for e in results.all()],
+        end_date=[e[0] for e in results.all()])
